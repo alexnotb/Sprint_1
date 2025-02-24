f00e8671ef8f98797b3ecb4ae41354cef787dc75
Oleksii Bezkibalnyi <alexeybezkibalnyi@gmail.com>
2025-02-24 16:55:06 -0330

    1

--- sprint1/server.js
+++ sprint1/server.js
@@ -72,10 +72,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'home.html'));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'home.html'));
-});
-
 // Auth status endpoint
 app.get('/auth/status', (req, res) => {
     const sessionPath = path.join(__dirname, 'data', 'sessions.json');
